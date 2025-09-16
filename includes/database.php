75cdd1dd9dc0a03f0edcdf29fd04fbdbe4a69606
onanymouse <50264820+onanymouse@users.noreply.github.com>
2025-09-16 18:55:30 +0700

    Create database.php

---
+++ includes/database.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'admin_dashboard';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi database gagal: " . $e->getMessage());
+}
+?>
